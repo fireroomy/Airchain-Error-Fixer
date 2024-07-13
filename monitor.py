0fcf360ed9ba587e402868de9e991b535fff3afa
fireroomy <fireroomy@gmail.com>
2024-07-14 00:06:28 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,8 +3,9 @@ import time
 
 journalctl_cmd = "sudo journalctl -u stationd -f --no-hostname -o cat"
 
-error_keywords = [" Switchyard client connection error", "Failed to Init VRF", " error in json rpc client, with http response metadata", "VRF record is nil", "Failed to get transaction by hash: not found", "Failed to"]
-command_to_run = "sudo systemctl restart stationd"
+error_keywords = [" Switchyard client connection error", "Failed to Init VRF", " error in json rpc client, with http response metadata", "VRF record is nil", "Failed to get transaction by hash: not found", "Failed to", "rpc error: code = Unknown desc = rpc error: code = Unknown desc = failed to execute message; message index: 0: rpc error: code = Unavailable desc = incorrect pod number [cosmos/cosmos-sdk@v0.50.3/baseapp/baseapp.go:991] with gas used: '110624': unknown request"
+]
+command_to_run = "sudo systemctl stop stationd && cd tracks && git pull && go run cmd/main.go rollback && go run cmd/main.go rollback && go run cmd/main.go rollback && sudo systemctl restart stationd && sudo journalctl -u stationd -f --no-hostname -o cat"
 
 def run_command(command):
     subprocess.run(command, shell=True)
